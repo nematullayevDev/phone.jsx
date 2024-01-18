87b1a3f61d43b2c58e94a2d43152e2562c13f5da
nematullayevDev <webdargon@gmail.com>
2024-01-18 22:50:21 +0500

    phone

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="phone">
+        <div className="window">
+          
+        </div>
+
+        <div className="numbers">
+          <button className="number_btn"><h2>1</h2></button>
+          <button className="number_btn"><h2>2</h2></button>
+          <button className="number_btn"><h2>3</h2></button>
+          <button className="number_btn"><h2>4</h2></button>
+          <button className="number_btn"><h2>5</h2></button>
+          <button className="number_btn"><h2>6</h2></button>
+          <button className="number_btn"><h2>7</h2></button>
+          <button className="number_btn"><h2>8</h2></button>
+          <button className="number_btn"><h2>9</h2></button>
+          <button className="number_btn"><h2>0</h2></button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
